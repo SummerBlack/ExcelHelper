a1b6f3fde9233d21e73c550f0294a25d14fd8e6c
SummerBlack <chengfen@mail.ustc.edu.cn>
2019-07-11 15:41:06 +0800

    封装的方便操作Excel文件的帮助类

---
+++ utils/excelhelper.cpp
@@ -0,0 +1,223 @@
+﻿#include "excelhelper.h"
+#include <QAxObject>
+#include <QDir>
+#include <QDebug>
+#include <QDateTime>
+
+ExcelHelper::ExcelHelper()
+{
+    excelApp = new QAxObject();
+
+    // 连接Excel控件
+    excelApp->setControl("Excel.Application");
+    // 不显示窗体
+    excelApp->dynamicCall("SetVisible(bool)","false");
+    // 不显示任何警告信息。如果为true那么在关闭时会出现类似“文件已修改，是否保存”的提示
+    excelApp->setProperty("DisplayAlerts", false);
+    // 获取工作簿集合
+    workBooks = excelApp->querySubObject("WorkBooks");
+}
+
+ExcelHelper::~ExcelHelper()
+{
+    closeExcel();
+}
+
+void ExcelHelper::openExcel(const QString &fileName)
+{
+    QFile file(fileName);
+    if(file.exists()){
+        workBook = workBooks->querySubObject("Open(const QString &)", fileName);
+    } else{
+        workBooks->dynamicCall("Add");
+        workBook = excelApp->querySubObject("ActiveWorkBook");
+    }
+    // 默认有一个sheet
+    workSheets = workBook->querySubObject("Sheets");
+    workSheet = workSheets->querySubObject("Item(int)", 1);
+}
+
+void ExcelHelper::setCellValue(int row, int column, const QVariant &value)
+{
+    QAxObject *cell = workSheet->querySubObject("Cells(int, int)", row, column);
+    cell->setProperty("Value", value);
+}
+
+QVariant ExcelHelper::getCellValue(int row, int column) const
+{
+    QAxObject *cell = workSheet->querySubObject("Cells(int, int)", row, column);
+    return cell->property("Value");
+}
+
+bool ExcelHelper::getTableValue(QList<QList<QVariant>> &value, const QString &range)
+{
+    QVariant var;
+    QAxObject *userRange;
+    if (range.isNull() || range.isEmpty()) {
+        userRange = workSheet->querySubObject("UsedRange");
+    }else {
+        userRange = workSheet->querySubObject("Range(const QString&)", range);
+    }
+
+    if (userRange == nullptr || userRange->isNull()) {
+        return false;
+    }
+
+    var = userRange->dynamicCall("Value");
+    value = castVariant2List(var);
+    return true;
+}
+
+bool ExcelHelper::setTableValue(QList<QList<QVariant> > &values, const int startRow, const int startColumn)
+{
+    qint64 startT = QDateTime::currentMSecsSinceEpoch();
+    if(values.size() <= 0){
+        return false;
+    }
+
+    if(NULL == workSheet || workSheet->isNull()){
+        return false;
+    }
+    bool succ = false;
+//    try {
+        // 行数
+        int row = values.size();
+        // 列数
+        int col = values.at(0).size();
+        // 起始单元,例如1，1最终转化为A1
+        QString rangeStart;
+        convertToColName(startColumn, rangeStart);
+        rangeStart += QString::number(startRow);
+        // 终点单元
+        QString rangeEnd;
+        convertToColName(startColumn + col - 1, rangeEnd);
+        rangeEnd += QString::number(startRow + row - 1);
+        // 单元范围
+        QString range = rangeStart + ":" + rangeEnd;
+
+        qDebug() << range;
+        QAxObject *usedRange = workSheet->querySubObject("Range(const QString&)", range);
+        if(NULL == usedRange || usedRange->isNull())
+        {
+            return false;
+        }
+
+        QVariant var = castList2Variant(values);
+        succ = usedRange->setProperty("Value", var);
+//    } catch (...) {
+//        qDebug() << "setTableValue Error!";
+//    }
+
+    qint64 endT = QDateTime::currentMSecsSinceEpoch();
+    qDebug() << "setTableValue: " << endT - startT;
+    return succ;
+}
+
+bool ExcelHelper::readFromFile(const QString &fileName, QList<QList<QVariant>> &value, const QString &range)
+{
+    QFile file(fileName);
+
+    if(!file.exists()){
+        return false;
+    }
+
+    workBook = workBooks->querySubObject("Open(const QString &)", fileName);
+    //默认有一个sheet
+    workSheets = workBook->querySubObject("Sheets");
+    workSheet = workSheets->querySubObject("Item(int)", 1);
+
+    bool result = getTableValue(value, range);
+    return result;
+}
+
+bool ExcelHelper::writeToFile(const QString &fileName, QList<QList<QVariant> > &values, const int startRow, const int startColumn)
+{
+    // 新建或打开文件
+    openExcel(fileName);
+    // 保存数据
+    bool success = setTableValue(values, startRow, startColumn);
+    // 保存excel文件
+    saveExcel(fileName);
+
+    return success;
+}
+
+QString ExcelHelper::convertToRangeName(int startRow, int startCol, int endRow, int endCol) const
+{
+    // 起始单元,例如1，1最终转化为A1
+    QString rangeStart;
+    convertToColName(startCol, rangeStart);
+    rangeStart += QString::number(startRow);
+    // 终点单元
+    QString rangeEnd;
+    convertToColName(endCol, rangeEnd);
+    rangeEnd += QString::number(endRow);
+    // 单元范围
+    QString range = rangeStart + ":" + rangeEnd;
+
+    qDebug() << range;
+    return range;
+}
+
+void ExcelHelper::getRange(int &startRow, int &startCol, int &rowCount, int &colCount) const
+{
+    QAxObject *usedrange = workSheet->querySubObject("UsedRange");//sheet范围
+    startRow = usedrange->property("Row").toInt();//起始行数
+    startCol = usedrange->property("Column").toInt(); //起始列数
+    QAxObject *rows, *columns;
+    rows = usedrange->querySubObject("Rows");//行
+    columns = usedrange->querySubObject("Columns");//列
+    rowCount = rows->property("Count").toInt();//行数
+    colCount = columns->property("Count").toInt();//列数
+    qDebug() << "startRow :" << startRow << "startCol" << startCol << "rowCount" << rowCount << "colCount" << colCount;
+}
+
+void ExcelHelper::saveExcel(const QString &fileName)
+{
+    //保存至filepath，注意一定要用QDir::toNativeSeparators将路径中的"/"转换为"\"，不然一定保存不了。
+    workBook->dynamicCall("SaveAs(const QString&)",QDir::toNativeSeparators(fileName));
+}
+
+void ExcelHelper::closeExcel()
+{
+    if (excelApp) {
+        //关闭工作簿
+        workBook->dynamicCall("Close()");
+        //关闭excel
+        excelApp->dynamicCall("Quit()");
+        delete excelApp;
+        excelApp = nullptr;
+    }
+}
+
+QList<QList<QVariant>> ExcelHelper::castVariant2List(QVariant &var) const
+{
+    QList<QList<QVariant>> list;
+    QList<QVariant> rows = var.toList();
+    for(auto row : rows) {
+        list.append(row.toList());
+    }
+    return list;
+}
+
+QVariant ExcelHelper::castList2Variant(QList<QList<QVariant>> &lists) const
+{
+    QVariantList vars;
+    for (auto list : lists){
+        vars.append(QVariant(list));
+    }
+    return QVariant(vars);
+}
+
+void ExcelHelper::convertToColName(int col, QString &res) const
+{
+    Q_ASSERT(col > 0 && col < 65535);
+    int tempData = col / 26;
+    if(tempData > 0){
+        int mode = col % 26;
+        convertToColName(mode, res);
+        convertToColName(tempData, res);
+    } else {
+        res = QString(col + 0x40) + res;
+    }
+}
---
+++ utils/excelhelper.h
@@ -0,0 +1,133 @@
+﻿#ifndef EXCELHELPER_H
+#define EXCELHELPER_H
+
+#include <QList>
+
+class QAxObject;
+/**
+ * @brief 该类为Excel表格操作的辅助类，简化对表格数据的读写操作
+ */
+class ExcelHelper
+{
+public:
+    ExcelHelper();
+    ~ExcelHelper();
+
+    /**
+     * @brief 打开/新建一个Excel,如果文件存在就打开，不存在就新建
+     * @param fileName 文件的名称
+     */
+    void openExcel(const QString &fileName);
+
+    /**
+     * @brief 设置表格中某一行某一列的值
+     * @param row
+     * @param column
+     * @param value
+     */
+    void setCellValue(int row, int column, const QVariant &value);
+
+    /**
+     * @brief 获取表格中某一行某一列的值
+     * @param row
+     * @param column
+     * @return
+     */
+    QVariant getCellValue(int row, int column) const;
+
+    /**
+     * @brief 查询Excel中的数据并保存到value中，如果指定查询范围就按照指定范围查，如果没有就查询表中所有数据
+     * @param value 返回数据保存在value中，其中QList<QVariant>标识一行数据
+     * @param range 查询的范围（如A1:Z10表示从第一行第一列到第10行第26列），默认为空，即查询所有
+     * @return 是否读取成功
+     */
+    bool getTableValue(QList<QList<QVariant>> &value, const QString &range = "");
+
+    /**
+     * @brief 将values中的数据保存到Excel中
+     * @param values 需要保存的数据
+     * @param startRow 数据的起始行
+     * @param startColumn 数据的起始列
+     * @return 是否保存成功
+     */
+    bool setTableValue(QList<QList<QVariant>> &values, const int startRow = 1, const int startColumn = 1);
+
+    /**
+     * @brief 指定Excel文件名，读取其中数据保存至value中
+     * @param fileName Excel文件名
+     * @param value 数据保存至value
+     * @param range 读取数据的范围
+     * @return 是否读取成功
+     */
+    bool readFromFile(const QString &fileName, QList<QList<QVariant>> &value, const QString &range = "");
+
+    /**
+     * @brief 将value数据写入到fileName文件中，文件如果不存在则新建
+     * @param fileName 保存数据的文件名
+     * @param values 数据
+     * @param startRow 保存数据的起始行
+     * @param startColumn 保存数据的起始列
+     * @return 是否成功
+     */
+    bool writeToFile(const QString &fileName, QList<QList<QVariant>> &values,const int startRow = 1, const int startColumn = 1);
+
+    /**
+     * @brief 将（1，1）->（10,26）转换为A1:Z10
+     * @param startRow 起始行
+     * @param startCol 起始列
+     * @param endRow 结束行
+     * @param endCol 结束列
+     * @return
+     */
+    QString convertToRangeName(int startRow, int startCol, int endRow, int endCol) const;
+
+    /**
+     * @brief 获取excel的起始行、起始列、行数和列数
+     * @param startRow 起始行
+     * @param startCol 起始列
+     * @param rowCount 行数
+     * @param colCount 列数
+     */
+    void getRange(int &startRow, int &startCol, int &rowCount, int &colCount) const;
+
+    /**
+     * @brief 保存文件
+     * @param fileName 文件名
+     */
+    void saveExcel(const QString &fileName);
+
+    /**
+     * @brief 关闭Excel，停止操作，关闭完了资源被清理了就不能再操作了，否则出错
+     */
+    void closeExcel();
+
+private:
+    /**
+     * @brief 将Excel中读取的文件QVariant类型转化为QList<QList<QVariant>>类型
+     * @param var
+     * @return QList<QVariant>表示一行数据
+     */
+    QList<QList<QVariant>> castVariant2List(QVariant& var) const;
+
+    /**
+     * @brief 将QList<QList<QVariant>>数据转换为Excel可以存储的QVariant类型
+     * @param lists
+     * @return
+     */
+    QVariant castList2Variant(QList<QList<QVariant>>& lists) const;
+
+    /**
+     * @brief 把列数转换为excel的字母列号，如1->A,2->B...26->Z,27->AA,28->AB...依此类推
+     * @param col 大于0的数
+     * @param res 字母列号，如1->A 26->Z 27->AA
+     */
+    void convertToColName(int col, QString &res) const;
+private:
+    QAxObject *excelApp = nullptr;
+    QAxObject *workBooks = nullptr;
+    QAxObject *workBook = nullptr;
+    QAxObject *workSheets = nullptr;
+    QAxObject *workSheet = nullptr;
+};
+
+#endif // EXCELHELPER_H
